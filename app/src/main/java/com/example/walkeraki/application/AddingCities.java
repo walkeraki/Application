9be91e6067639c50e83d18a854b575533a593776
walkeraki <jurejure0@gmail.com>
2015-11-18 21:44:14 +0100

    ...

--- app/src/main/java/com/example/walkeraki/application/AddingCities.java
+++ app/src/main/java/com/example/walkeraki/application/AddingCities.java
@@ -2,20 +2,12 @@ package com.example.walkeraki.application;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.Context;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.content.Intent;
-import android.widget.Toast;
-import android.content.Intent;
-import android.graphics.Color;
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -31,8 +23,6 @@ public class AddingCities extends Activity {
     private Context context;
     ArrayList<String> list = new ArrayList<String>();
 
-
-
     public void sendMessage(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
@@ -45,10 +35,29 @@ public class AddingCities extends Activity {
         set.add(message);
         editor.putStringSet("keys", set);
         editor.apply();
+        startActivity(intent);
+    }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adding_cities);
+        input = (EditText) findViewById(R.id.editText);
+        next = (Button) findViewById(R.id.button);
+        //Toast.makeText(getApplicationContext(), ""+value, Toast.LENGTH_LONG).show();
+        next.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage(v);
+            }
+        });
+
+    }
 
 
 
-        /*SharedPreferences sharedPref = getSharedPreferences("tocke",MODE_PRIVATE);
+}
+  /*SharedPreferences sharedPref = getSharedPreferences("tocke",MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putInt("tocke", value);
         editor.commit();
@@ -75,30 +84,10 @@ public class AddingCities extends Activity {
         */
 
 
-        //intent.putStringArrayListExtra("array", list);
-        //intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
+//intent.putStringArrayListExtra("array", list);
+//intent.putExtra(EXTRA_MESSAGE, message);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.adding_cities);
-        input = (EditText) findViewById(R.id.editText);
-        next = (Button) findViewById(R.id.button);
-        //Toast.makeText(getApplicationContext(), ""+value, Toast.LENGTH_LONG).show();
-        next.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendMessage(v);
-            }
-        });
-        /*SharedPreferences prefs = getSharedPreferences("mesto", MODE_PRIVATE);
+/*SharedPreferences prefs = getSharedPreferences("mesto", MODE_PRIVATE);
         String[] tocke = prefs.getString("mesto", "");
         Log.e("tocke", tocke + "");
         */
-    }
-
-
-
-}
--- app/src/main/java/com/example/walkeraki/application/MainActivity.java
+++ app/src/main/java/com/example/walkeraki/application/MainActivity.java
@@ -1,39 +1,15 @@
 package com.example.walkeraki.application;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,16 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //Intent intent = getIntent();
-        //list = intent.getStringArrayListExtra("array");
-
-       /* SharedPreferences sharedPref = getSharedPreferences("tocke",MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt("tocke", 1000);
-        editor.commit();
-        */
-
         next = (Button) findViewById(R.id.button2);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,15 +31,9 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-        // Intent intent = getIntent();
-        //String message = intent.getStringExtra(AddingCities.EXTRA_MESSAGE);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
-
         mRecyclerView.setHasFixedSize(true);
-
-
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
@@ -83,12 +43,10 @@ public class MainActivity extends AppCompatActivity {
         set = sharedPref.getStringSet("keys", new HashSet<String>());
         ArrayList<String> pojos = new ArrayList<>();
 
-
         for (String str : set)
             pojos.add(str);
 
         mAdapter = new MyAdapter(pojos, this);
-
         mRecyclerView.setAdapter(mAdapter);
 
         //network
@@ -100,3 +58,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+//Intent intent = getIntent();
+//list = intent.getStringArrayListExtra("array");
+
+       /* SharedPreferences sharedPref = getSharedPreferences("tocke",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("tocke", 1000);
+        editor.commit();
+        */
